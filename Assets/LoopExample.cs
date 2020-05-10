2504062c166390b0f0a45ec14c5f62a590da8fc4
ahmad al asal <ahmad.a@genie-soft.com>
2020-05-10 14:07:27 +0300

    Init

---
+++ Assets/LoopExample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopExample : MonoBehaviour
+{
+
+    void Start()
+    {
+        LoopFloat angle = new LoopFloat(0, 0, 360);
+        angle += 30f;
+        Debug.Log(angle);
+        angle -= 60f;
+        Debug.Log(angle);
+        angle += 1380f;
+        Debug.Log(angle);
+        angle.SetValue(30f);
+        Debug.Log(angle.Delta(330f));
+
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = i + 1;
+        }
+
+        LoopInt indexer = new LoopInt(0, 0, numbers.Length);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(numbers[indexer++]);
+        }
+
+    }
+}
